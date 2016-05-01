a9e7187df608d9725e647f0bc0db398652882324
Nathan <natethepcspecialist@gmail.com>
2016-04-30 22:38:35 -0400

    'update'

--- app.js
+++ app.js
@@ -17,31 +17,33 @@ app.get('/', function(req, res) {
 
 //Lib
 var Player = require('./player'),
-	grid = require('./lib/grid');
+	config = require('./lib/config'),
+	requirejsConfig = require('./requirejs-config'),
+	grid = requirejs('grid')(config);
 
 var players = [];
 io.on('connection', function(socket) {
 	console.log("Socket connected", socket.id);
 
-	// socket.emit('canvas', config.gridSize, config.sectionSize);
-	// socket.emit('grid', grid.gridSegments);
+	socket.emit('canvas', config.gridSize, config.sectionSize);
+	socket.emit('grid', grid.getAllSegmentsCoords());
 
-	// // Create player and add to collection
-	// socket.player = new Player(
-	// 	socket,
-	// 	grid,
-	// 	Math.random() * 400,
-	// 	Math.random() * 400
-	// );
-	// players.push(socket.player);
-	// socket.player.updateGridLocation();
+	// Create player and add to collection
+	socket.player = new Player(
+		socket,
+		grid,
+		Math.random() * 400,
+		Math.random() * 400
+	);
+	players.push(socket.player);
+	socket.player.init(io);
 
-	// socket.on('move', function(dir) {
-	// 	socket.player.move(dir);
-	// });
-	// socket.on('stopmove', function() {
-	// 	socket.player.stopmove();
-	// });
+	socket.on('move', function(dir) {
+		socket.player.move(dir);
+	});
+	socket.on('stopmove', function() {
+		socket.player.stopmove();
+	});
 	// socket.on('shoot', function() {
 	// 	socket.player.shoot();
 	// });
@@ -54,5 +56,4 @@ io.on('connection', function(socket) {
 setInterval(function() {
 	for(var i = 0, total = players.length; i < total; i++)
 		players[i].tick(io);
-	grid.tick();
 }, 1000 / 60);
--- player.js
+++ player.js
@@ -6,67 +6,95 @@ function Player(socket, grid, x, y) {
 	this.grid = grid;
 	this.speed = 2;
 	this.frame = 0;
+	this.x = x;
+	this.y = y;
+	this.w = 10;
+	this.h = 10;
 	this.type = 'player';
+	this.segmentAreaType = '3x3';
 	this.bullets = [];
 	this.frames = 0;
 	this.facingDir = 'right';
-	this.item = grid.createItem(this, x, y, 10, 10);
+	this.item = grid.createItem(this);
 }
 
 Player.prototype = {
+	init : function(io) {
+		var self = this;
+		this.updateGridLocation(io);
+		this.item.subscribeToArea();
+
+		// this.grid.subscribeSegmentArea(this.item.getSegment(), 
+									  // this.item.segmentArea, this.id, this.areaSegmentOnChange.bind(this));
+	},
 	shoot : function() {
 		var bullet = new Bullet(this.id, this.bullets.length, this.grid, this.gridNum, this.coords, this.facingDir);
 		bullet.init();
 		this.bullets.push(bullet);
 	},
+	areaSegmentOnChange : function(event, item, segment) {
+		this.socket.emit(event, item.client());
+	},
 	tick : function(io) {
 		this.frames++;
 		if(this.movedir === 'right')
-			this.coords.x += this.speed;
+			this.x += this.speed;
 		if(this.movedir === 'left')
-			this.coords.x -= this.speed;
+			this.x -= this.speed;
 		if(this.movedir === 'up')
-			this.coords.y -= this.speed;
+			this.y -= this.speed;
 		if(this.movedir === 'down')
-			this.coords.y += this.speed;
+			this.y += this.speed;
 
-		for(var i = 0; i < this.bullets.length; i++){
-			this.bullets[i].tick();
-			if(!this.bullets[i].destroyed){
-				console.log(this.bullets[i].client());
-				io.in(this.gridName).emit('moved', this.bullets[i].client());
-			}
-		}
+		// for(var i = 0; i < this.bullets.length; i++){
+		// 	this.bullets[i].tick();
+		// 	if(!this.bullets[i].destroyed){
+		// 		io.in(this.gridName).emit('moved', this.bullets[i].client());
+		// 	}
+		// }
 
-		this.bullets = _.reject(this.bullets, function(bullet){ return bullet.destroyed; });
+		// this.bullets = _.reject(this.bullets, function(bullet){ return bullet.destroyed; });
 		if(this.movedir){
-			this.updateGridLocation();
-			io.in(this.gridName).emit('moved', this.client());
+			this.item.move(this);
+			this.updateGridLocation(io);
+			// io.in(this.gridName).emit('moved', this.item.client());
+		}
+		if(this.udpatedSegment){
+			this.udpatedSegment = false;
+			// this.emitToRoom(io);
+			this.emitSegmentArea(io);
 		}
 	},
-	updateGridLocation : function() {
-		var gridCoords = this.item.getSegmentCoords(this.coords);
+	updateGridLocation : function(io) {
+		var gridCoords = this.item.getCurrentSegmentCoords();
 
-		if(gridCoords === this.gridCoords)return;
-		this.frame++;
+		if(this.gridCoords && this.gridCoords[0] === gridCoords[0] && this.gridCoords[1] === gridCoords[1]){
+			return;
+		}
 
-		if(this.gridCoords !== undefined)
-			this.grid.removeItem(this);
+		if(!this.gridCoords)this.gridCoords = gridCoords;
 
+		this.emitToRoom(io);
+		this.socket.leave(this.gridName);
 		this.gridCoords = gridCoords;
 		this.gridName = 'grid-' + gridCoords.join('-');
-		console.log(this.gridName);
-
 		this.socket.join(this.gridName);
-		this.grid.addItem(this);
+		this.udpatedSegment = true;
 	},
-	
-	client : function() {
-		return {
-			id : this.id,
-			type : this.type,
-			coords : this.coords
-		}
+	emitToRoom : function(io) {
+		var segment = this.grid.getSegment(this.gridCoords[0], this.gridCoords[1]);
+		io.in(this.gridName).emit('items', segment.clientItems());
+	},
+	emitSegment : function(segment) {
+		this.socket.emit('segment items', segment.clientItems());	
+	},
+	emitSegmentArea : function() {
+		var segments = this.grid.getAllSegmentsInSegmentArea(this.item.getSegment(), this.item.segmentArea),
+			items = [];
+		segments.forEach(function(segment) {
+			items.push(segment.clientItems());
+		});
+		this.socket.emit('items', _.flatten(items));
 	},
 	move : function(dir) {
 		this.movedir = dir;
